b44dbed4c65dbcb6f904dd5e4de6856726b43232
danielwiener <daniel@danielwiener.com>
2012-01-05 16:20:22 -0500

    working on getting the header (which is wider than the page) to fit and work as a background and moving the menu to work with the header, line up, etc. And changed color of the dropdown menus

--- themes/tadhills/functions.php
+++ themes/tadhills/functions.php
@@ -99,25 +99,9 @@ endif;
 
 if ( ! function_exists( 'twentyten_admin_header_style' ) ) :
 /**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * Referenced via add_custom_image_header() in twentyten_setup().
- *
- * @since Twenty Ten 1.0
+ * DW deleted - Styles the header image displayed on the Appearance > Header admin panel.
  */
 function twentyten_admin_header_style() {
-?>
-<style type="text/css">
-/* Shows the same border as on front end */
-#headimg {
-	border-bottom: 1px solid #000;
-	border-top: 4px solid #000;
-}
-/* If NO_HEADER_TEXT is false, you would style the text with these selectors:
-	#headimg #name { }
-	#headimg #desc { }
-*/
-</style>
-<?php
+// do nothing 
 }
 endif;
